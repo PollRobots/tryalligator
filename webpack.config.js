89bfb1a6ed6e24ddc5e3eda0f7d9d34daf815f34
Paul C Roberts <pacaro@gmail.com>
2022-03-26 13:20:57 -0700

    hello world

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserWebpack = require("terser-webpack-plugin");
+const { CleanWebpackPlugin: CleanWebpack } = require("clean-webpack-plugin");
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+  const template = "./index.html";
+
+  const distPath = path.resolve(__dirname, "dist");
+
+  return {
+    entry: "./src/index.tsx",
+    output: {
+      path: distPath,
+      filename: "[name].[contenthash].js",
+    },
+    devServer: {
+      port: 8080,
+      host: "0.0.0.0",
+      static: {
+        publicPath: distPath,
+      },
+    },
+    devtool: "source-map",
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+    },
+
+    optimization: {
+      minimize: isProduction,
+      minimizer: [new TerserWebpack()],
+      realContentHash: true,
+      splitChunks: {
+        chunks: "all",
+      },
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          exclude: /node_modules/,
+          use: "ts-loader",
+        },
+        {
+          enforce: "pre",
+          test: /\.js$/,
+          loader: "source-map-loader",
+        },
+      ],
+    },
+
+    plugins: [
+      new CleanWebpack(),
+      new HtmlWebpackPlugin({
+        title: "Try Alligator",
+        filename: "tryalligator.html",
+        template: template,
+      }),
+    ],
+  };
+};
